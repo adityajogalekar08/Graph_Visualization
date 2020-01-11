2129bd6881d2e32db280ee9ee300e63b5bf925e1
Aditya V Jogalekar <adiiitya08@gmail.com>
2020-01-11 19:55:12 +0100

    trying to label more than 2 nodes but label or node name not coming, trying to fix that

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Button,Form, FormControl,Row,Container,Col } from 'react-bootstrap';
-import {Sigma, RandomizeNodePositions, RelativeSize} from 'react-sigma';
+import {Sigma, RandomizeNodePositions,LoadGEXF,Filter,ForceAtlas2, RelativeSize, LoadJSON, EdgeShapes, NodeShapes} from 'react-sigma';
 
 
 function App() {
@@ -15,14 +15,21 @@ function App() {
     </Navbar>
     <Container>
     <Row>
-    <Col>1 of 2</Col>
-    <Col className="sigma_col" id="sigma_center">
-        <Sigma graph={{nodes:[{id:"n1", label:"Alice"}, {id:"n2", label:"Rabbit"},{id:"n3", label:"elf"}], edges:[{id:"e1",source:"n1",target:"n2",label:"SEES"},{id:"e2",source:"n3",target:"n1",label:"SAW"}]}} settings={{drawEdges:true},{drawLabels:true}}>
+    <Col>
+    <Button variant="primary" className="simple_btn" target="#sigma_center">Primary</Button>
+    </Col>
+    <Col className="sigma_col" id="sigma_center" xs={5}>
+        <Sigma graph={{nodes:[{id:"n1", label:"Alice"}, {id:"n2", label:"Rabbit"}], edges:[{id:"e1",source:"n1",target:"n2",label:"SEES"}]}} settings={{drawEdges:true},{drawLabels:true}}>
             <RelativeSize initialSize={15}/>
             <RandomizeNodePositions/>
         </Sigma>
     </Col>
-    <Col>1 of 2</Col>
+    <Col xs={5}>
+    <Sigma graph={{nodes:[{id:"n1", label:"Somesh"},{id:"n3", label:"Aditya"}, {id:"n2", label:"Rabbit"}], edges:[{id:"e1",source:"n1",target:"n3",label:"SEES"},{id:"e2",source:"n3",target:"n2",label:"SEES"}]}} settings={{drawEdges:true},{showLabels:true}}>
+            <RelativeSize initialSize={500}/>
+            <RandomizeNodePositions/>
+        </Sigma>
+    </Col>
     </Row>
 
     </Container>
