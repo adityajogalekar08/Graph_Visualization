1e1b91ab748c87a92828b0f7aceba12d5d9c062b
Aditya V Jogalekar <adiiitya08@gmail.com>
2020-01-11 17:35:39 +0100

    simple node and label test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,14 +9,19 @@ import {Sigma, RandomizeNodePositions, RelativeSize} from 'react-sigma';
 function App() {
   return (
     <>
-
+  
     <Navbar className="navbar_bg">
-    <Navbar.Brand href="#home" className="brand">Graph Visualization</Navbar.Brand>
+    <Navbar.Brand className="brand">Graph Visualization</Navbar.Brand>
     </Navbar>
-
+    <Sigma graph={{nodes:[{id:"n1", label:"Alice"}, {id:"n2", label:"Rabbit"}], edges:[{id:"e1",source:"n1",target:"n2",label:"SEES"}]}} settings={{drawEdges:true}}>
+        <RelativeSize initialSize={15}/>
+        <RandomizeNodePositions/>
+    </Sigma>
+    
     <Navbar fixed="bottom" className="footer" />
   </>
   );
 }
+
 
 export default App;
